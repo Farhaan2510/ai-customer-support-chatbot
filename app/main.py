5839de58ea122e06ed80f5a2623774f52f868923
Farhaan2510 <farhaansyed98@outlook.com>
2026-08-16 00:03:04 +0530

    Integrate Groq LLM with chat endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel  
+from dotenv import load_dotenv
+from openai import OpenAI
+
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
+)
+
+API_KEY = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 name = "Farhaan"
@@ -17,6 +30,23 @@ def greet(name):
 
 @app.post("/chat")
 def chat(request: ChatRequest):
-    return {
-        "reply": f"You said: {request.message}"
-    }
+    try:
+
+        response = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {
+                    "role": "user",
+                    "content": request.message
+                }
+            ]
+        )
+
+        return {
+            "reply": response.choices[0].message.content
+        }
+    
+    except Exception as e:
+        return{
+            "error": str(e)
+        }
